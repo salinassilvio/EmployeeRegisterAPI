84f58414ae9fef27566e0657b780e57775847d05
Silvio Salinas <ssalinas.silvio@gmail.com>
2021-11-15 20:55:22 -0600

    Add project files.

---
+++ EmployeeRegisterAPI/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegisterAPI.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Column(TypeName ="nvarchar(50)")]
+        public string EmployeeName { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Ocupation { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+    }
+}
